c65705615f8d4271d378012119d1cfd39fbb7c8d
NimmiKChandran27 <mca536@rajagiri.edu>
2021-05-31 07:51:25 +0530

    first commit

---
+++ resources/views/aboutus.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="/">HOTEL ARRYAS</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/menu">MENU</a>
+                  </li>
+                  
+                  
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/aboutus" tabindex="-1" aria-disabled="true">About Us</a>
+                  </li>
+                </ul>
+                <ul class="list-unstyled" style="font-size: 20px;color: rgb(243, 247, 243);">
+                    <li>Phone: 91 - 484 - 5839869</li>
+                    <li>Fax: 91 - 484 - 349208+</li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+
+
+<br>
+<center>
+<b><u>Arryas Luxury Hotel</u></b>
+<br>
+Arryas a Business class Luxury hotel with lot of aspiration and pride is all set, at the new business hub of cochin, Edappally. Entry to NH 17 highway service road, Adjacent to Lulu corporate office, Opposite to Al Shifa hospital and Al Ameen public School, Edappally. Designed carefully to serve both Business and Leisure sector. We are at walk able distance to LULU mall, Edappally metro station and accessible easily to other parts of Cochin city
+
+Aryyas means cluster of stars, it’s to delight and embrace all with warmth and happiness. We welcome all to enjoy and cherish our hospitality.</center>
+
+
+<img src="https://www.tajhotels.com/content/dam/gateway/hotels/Beach-Road,Calicut/images/eat-and-drink/Logan%27sBar4x3.jpg/jcr:content/renditions/cq5dam.web.756.756.jpeg" class="img-fluid" alt="..."></center>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script
+</body>
+</html>
---
+++ resources/views/menu.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+</head>
+<body>
+    
+
+
+
+
+<<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="/">HOTEL ARRYAS</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/menu">MENU</a>
+                  </li>
+                  
+                  
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/aboutus" tabindex="-1" aria-disabled="true">About Us</a>
+                  </li>
+                </ul>
+                <ul class="list-unstyled" style="font-size: 20px;color: rgb(243, 247, 243);">
+                    <li>Phone: 91 - 484 - 5839869</li>
+                    <li>Fax: 91 - 484 - 349208+</li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+
+<br>
+<br>
+
+          <div class="container">
+        
+        <div class="row">
+            <div class="col col-12 col-sm-12 col-md-12"></div>
+
+
+
+            <div class="accordion" id="accordionPanelsStayOpenExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+        Breakfast
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+      <div class="accordion-body">
+        <strong>
+        <ul class="list-group">
+  
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    IDILI SAMBAR
+    <span class="badge bg-primary rounded-pill">Rs.60</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    DOSA SAMBAR
+    <span class="badge bg-primary rounded-pill">Rs.70</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    UPPMA
+    <span class="badge bg-primary rounded-pill">Rs.30</span>
+  </li>
+</ul>.
+</strong> Breakfast is often called 'the most important meal of the day', and for good reason. As the name suggests, breakfast breaks the overnight fasting period. It replenishes your supply of glucose to boost your energy levels and alertness, while also providing other essential nutrients required for good health.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+        LUNCH
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
+      <div class="accordion-body">
+      <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+      <div class="accordion-body">
+        <strong>
+        <ul class="list-group">
+  
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    RICE MEALS
+    <span class="badge bg-primary rounded-pill">Rs.60</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    NORTH THALI
+    <span class="badge bg-primary rounded-pill">Rs.70</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    BIRIYANI
+    <span class="badge bg-primary rounded-pill">Rs.60</span>
+  </li>
+</ul>.
+</strong> It provides energy and nutrients to keep the body and brain working through the afternoon. According to experts, lunch provides nourishment to the body and brain and reduces stress and eating lunch provides a break from the activities of the day and gives energy for the rest of the afternoon
+      </div>
+    </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="panelsStayOpen-headingThree">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+        DINNER
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
+      <div class="accordion-body">
+      <ul class="list-group">
+  
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    RICE MEALS
+    <span class="badge bg-primary rounded-pill">Rs.60</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    CHAPATHI AND-CHICKEN,BEEF
+    <span class="badge bg-primary rounded-pill">Rs.70</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    POROTTA-CHICKEN-BEEF
+    <span class="badge bg-primary rounded-pill">Rs.60</span>
+  </li>
+</ul>.
+</strong> It provides energy and nutrients to keep the body and brain working through the afternoon. According to experts, lunch provides nourishment to the body and brain and reduces stress and eating lunch provides a break from the activities of the day and gives energy for the rest of the afternoon
+      </div>
+    </div>
+    </div>
+
+    </div>
+  </div>
+</div>
+
+
+
+
+            
+           
+</div>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+</html>
